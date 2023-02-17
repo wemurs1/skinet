0f4f143c872551658d6a7d3b7f62783e50cda4f4
Stuart Murray <stuart.murray.dev@outlook.com>
2023-02-17 16:24:11 +0000

    checkpoint

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+      complete: () => {
+        console.log('request compled');
+        console.log('extra statements');
+      },
+    });
+  }
 }
