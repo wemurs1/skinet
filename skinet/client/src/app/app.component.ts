40413ad959454128de45e3c383f050a30faef149
Stuart Murray <stuart.murray.dev@outlook.com>
2023-02-17 17:14:04 +0000

    interface for Product

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe({
-      next: (response) => console.log(response),
-      error: (error) => console.log(error),
-      complete: () => {
-        console.log('request compled');
-        console.log('extra statements');
-      },
-    });
+    this.http
+      .get<Pagination<Product[]>>(
+        'http://localhost:5001/api/products?pageSize=50'
+      )
+      .subscribe({
+        next: (response) => (this.products = response.data),
+        error: (error) => console.log(error),
+        complete: () => {
+          console.log('request compled');
+          console.log('extra statements');
+        },
+      });
   }
 }
