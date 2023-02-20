524fbba2d15d1390aea0e92416fb8e660f5a86d4
Stuart Murray <stuart.murray.dev@outlook.com>
2023-02-20 13:08:30 +0000

    refactor client and add basic product page

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Pagination } from './models/pagination';
-import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +7,8 @@ import { Product } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get<Pagination<Product[]>>(
-        'http://localhost:5001/api/products?pageSize=50'
-      )
-      .subscribe({
-        next: (response) => (this.products = response.data),
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('request compled');
-          console.log('extra statements');
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
