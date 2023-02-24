8c836d39e70fb9b44533c588b79954ae37efc14f
Stuart Murray <stuart.murray.dev@outlook.com>
2023-02-24 17:50:45 +0000

    checkpoint

--- skinet/client/src/app/shared/shared.module.ts
+++ skinet/client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [OrderTotalsComponent],
   imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
-  exports: [PaginationModule, CarouselModule],
+  exports: [PaginationModule, CarouselModule, OrderTotalsComponent],
 })
 export class SharedModule {}
