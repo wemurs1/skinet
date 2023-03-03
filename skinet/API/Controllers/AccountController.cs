ef96a534535adcbc2f7550299875180d01a87f1d
Stuart Murray <stuart.murray.dev@outlook.com>
2023-03-03 08:43:58 +0000

    account controller issues tokens

--- skinet/API/Controllers/AccountController.cs
+++ skinet/API/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
+using System.Security.Claims;
 using API.Dtos;
 using API.Errors;
 using Core.Entities.Identity;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +12,33 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly ITokenService _tokenService;
         private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public AccountController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            ITokenService tokenService
+        )
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            var user = await _userManager.FindByEmailAsync(email!);
+
+            return new UserDto
+            {
+                Email = user!.Email,
+                DisplayName = user.DisplayName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
@@ -25,12 +50,33 @@ namespace API.Controllers
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password!, false);
             if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
 
+            return new UserDto
+            {
+                Email = user.Email,
+                DisplayName = user.DisplayName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password!);
+
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
 
             return new UserDto
             {
                 Email = user.Email,
                 DisplayName = user.DisplayName,
-                Token = "This will be a token"
+                Token = _tokenService.CreateToken(user)
             };
         }
     }
--- skinet/API/Extensions/ApplicationServicesExtensions.cs
+++ skinet/API/Extensions/ApplicationServicesExtensions.cs
@@ -2,6 +2,7 @@ using API.Errors;
 using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
@@ -27,6 +28,7 @@ namespace API.Extensions
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ITokenService, TokenService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ApiBehaviorOptions>(options =>
             {
