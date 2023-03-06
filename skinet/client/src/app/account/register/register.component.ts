f9feefb1ba390b850e07e7902eed236d80c88a30
Stuart Murray <stuart.murray.dev@outlook.com>
2023-03-06 18:12:05 +0000

    client side validation

--- skinet/client/src/app/account/register/register.component.ts
+++ skinet/client/src/app/account/register/register.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(
+    private formBuilder: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
 
+  complexPassword =
+    "(?=^.{6,10}$)(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*s).*$";
+
+  registerForm = this.formBuilder.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [Validators.required, Validators.pattern(this.complexPassword)],
+    ],
+  });
+
+  onSubmit() {
+    this.accountService
+      .register(this.registerForm.value)
+      .subscribe({ next: () => this.router.navigateByUrl('/shop') });
+  }
 }
