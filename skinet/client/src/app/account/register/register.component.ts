ccd0db3285fa728073e60c5643f1edc6def287a5
Stuart Murray <stuart.murray.dev@outlook.com>
2023-03-07 11:47:34 +0000

    debounce email check to reduce API traffic

--- skinet/client/src/app/account/register/register.component.ts
+++ skinet/client/src/app/account/register/register.component.ts
@@ -6,7 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { finalize, map } from 'rxjs';
+import { debounceTime, finalize, map, switchMap, take } from 'rxjs';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -48,9 +48,15 @@ export class RegisterComponent {
 
   validateEmailNotTaken(): AsyncValidatorFn {
     return (control: AbstractControl) => {
-      return this.accountService.checkEmailExists(control.value).pipe(
-        map((result) => (result ? { emailExists: true } : null)),
-        finalize(() => control.markAllAsTouched())
+      return control.valueChanges.pipe(
+        debounceTime(1000),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map((result) => (result ? { emailExists: true } : null)),
+            finalize(() => control.markAllAsTouched())
+          );
+        })
       );
     };
   }
