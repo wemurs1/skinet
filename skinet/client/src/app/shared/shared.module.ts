ab2f4567db5a0fb256c369a998bf0dfbf375aa32
Stuart Murray <stuart.murray.dev@outlook.com>
2023-03-14 15:49:19 +0000

    client caching

--- skinet/client/src/app/shared/shared.module.ts
+++ skinet/client/src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { StepperComponent } from './components/stepper/stepper.component';
 import { BasketSummaryComponent } from './basket-summary/basket-summary.component';
 import { RouterModule } from '@angular/router';
+import { PagerComponent } from './pager/pager.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +18,14 @@ import { RouterModule } from '@angular/router';
     TextInputComponent,
     StepperComponent,
     BasketSummaryComponent,
+    PagerComponent,
   ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
     ReactiveFormsModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
     CdkStepperModule,
     RouterModule,
@@ -37,6 +40,7 @@ import { RouterModule } from '@angular/router';
     StepperComponent,
     CdkStepperModule,
     BasketSummaryComponent,
+    PagerComponent,
   ],
 })
 export class SharedModule {}
--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { Brand } from '../shared/models/brand';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
@@ -15,23 +15,38 @@ export class ShopService {
   products: Product[] = [];
   brands: Brand[] = [];
   types: Type[] = [];
+  pagination?: Pagination<Product[]>;
+  shopParams = new ShopParams();
+  productCache = new Map<string, Pagination<Product[]>>();
 
   constructor(private http: HttpClient) {}
 
-  getProducts(shopParams: ShopParams) {
+  getProducts(useCache = true): Observable<Pagination<Product[]>> {
+    if (!useCache) this.productCache = new Map();
+
+    if (this.productCache.size > 0 && useCache) {
+      if (this.productCache.has(Object.values(this.shopParams).join('-'))) {
+        this.pagination = this.productCache.get(
+          Object.values(this.shopParams).join('-')
+        );
+        if (this.pagination) return of(this.pagination);
+      }
+    }
+
     let params = new HttpParams();
 
-    if (shopParams.brandId > 0)
-      params = params.append('brandId', shopParams.brandId);
+    if (this.shopParams.brandId > 0)
+      params = params.append('brandId', this.shopParams.brandId);
 
-    if (shopParams.typeId > 0)
-      params = params.append('typeId', shopParams.typeId);
+    if (this.shopParams.typeId > 0)
+      params = params.append('typeId', this.shopParams.typeId);
 
-    if (shopParams.search) params = params.append('search', shopParams.search);
+    if (this.shopParams.search)
+      params = params.append('search', this.shopParams.search);
 
-    params = params.append('sort', shopParams.sort);
-    params = params.append('pageIndex', shopParams.pageNumber);
-    params = params.append('pageSize', shopParams.pageSize);
+    params = params.append('sort', this.shopParams.sort);
+    params = params.append('pageIndex', this.shopParams.pageNumber);
+    params = params.append('pageSize', this.shopParams.pageSize);
 
     return this.http
       .get<Pagination<Product[]>>(this.baseUrl + 'products', {
@@ -39,15 +54,35 @@ export class ShopService {
       })
       .pipe(
         map((response) => {
-          this.products = response.data;
+          this.productCache.set(
+            Object.values(this.shopParams).join('-'),
+            response
+          );
+          this.pagination = response;
           return response;
         })
       );
   }
 
+  setShopParams(params: ShopParams) {
+    this.shopParams = params;
+  }
+
+  getShopParams() {
+    return this.shopParams;
+  }
+
   getProduct(id: number) {
-    const product = this.products.find((p) => p.id === id);
-    if (product) return of(product);
+    const product = [...this.productCache.values()].reduce(
+      (acc, paginatedResult) => {
+        return {
+          ...acc,
+          ...paginatedResult.data.find((x) => x.id === id),
+        };
+      },
+      {} as Product
+    );
+    if (Object.keys(product).length !== 0) return of(product);
     return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
 
