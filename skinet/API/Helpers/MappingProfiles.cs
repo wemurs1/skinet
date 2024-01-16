cfc6d2dfbdbd49a285b8cd040d3429ea29f308b7
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-16 16:34:45 +0000

    use resolver for picture url to add host info

--- skinet/API/Helpers/MappingProfiles.cs
+++ skinet/API/Helpers/MappingProfiles.cs
@@ -10,6 +10,7 @@ public class MappingProfiles : Profile
     {
         CreateMap<Product, ProductToReturnDto>()
             .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand!.Name))
-            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType!.Name));
+            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType!.Name))
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
     }
 }
