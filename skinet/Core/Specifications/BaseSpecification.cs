54298cdc6df67a0f0b6371c0ad83652ef1450f4e
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-16 14:28:03 +0000

    specification pattern

---
+++ skinet/Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Specifications;
+
+public class BaseSpecification<T> : ISpecification<T>
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public BaseSpecification()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+    }
+
+    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+
+    public Expression<Func<T, bool>> Criteria { get; }
+
+    public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+}
