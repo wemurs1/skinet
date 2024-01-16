816dbdd9e4a4dfb5e1ee3592bf3518b0ff24cc04
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-16 11:31:14 +0000

    add seed data

--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,4 +12,12 @@ public class StoreContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
---
+++ skinet/Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Core.Entities;
+
+namespace Infrastructure.Data;
+
+public class StoreContextSeed
+{
+    public static async Task SeedAsync(StoreContext context)
+    {
+        if (!context.ProductBrands.Any())
+        {
+            var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+            var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+            if (brands != null) context.ProductBrands.AddRange(brands);
+        }
+
+        if (!context.ProductTypes.Any())
+        {
+            var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+            var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+            if (types != null) context.ProductTypes.AddRange(types);
+        }
+
+        if (!context.Products.Any())
+        {
+            var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+            if (products != null) context.Products.AddRange(products);
+        }
+
+        if (context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
+    }
+}
