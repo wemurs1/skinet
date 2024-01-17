77555cf4a157532eb4288029fbb8b8d785c0a798
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-17 14:38:53 +0000

    annotate api responses for swagger

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -36,12 +37,14 @@ public class ProductsController : BaseApiController
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto?>> GetProduct(int id)
     {
         var spec = new ProductsWithTypesAndBrandsSpecification(id);
         var product = await _productsRepo.GetEntityWithSpec(spec);
 
-        if (product == null) return NotFound();
+        if (product == null) return NotFound(new ApiResponse(404));
 
         return _mapper.Map<Product, ProductToReturnDto>(product);
     }
