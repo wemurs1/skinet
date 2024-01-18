1e6effd7d0141ecb906fd184663fbae58582381f
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-18 12:44:30 +0000

    search

--- skinet/Core/Specifications/ProductSpecParams.cs
+++ skinet/Core/Specifications/ProductSpecParams.cs
@@ -13,4 +13,10 @@ public class ProductSpecParams
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
     public string? Sort { get; set; }
+    private string? _search;
+    public string? Search
+    {
+        get => _search;
+        set => _search = value!.ToLower();
+    }
 }
--- skinet/Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ skinet/Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,6 +6,7 @@ public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product
 {
     public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
         : base(x =>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name!.ToLower().Contains(productParams.Search)) &&
             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
             (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
         )
