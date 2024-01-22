1c6088d41319b018e3c0b49e7609ebf0b21d3867
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-22 14:59:11 +0000

    client search

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -19,6 +19,7 @@ export class ShopService {
 
     if (shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
     if (shopParams.typeId > 0) params = params.append('typeId', shopParams.typeId);
+    if (shopParams.search) params = params.append('search', shopParams.search);
     params = params.append('sort', shopParams.sort);
     params = params.append('pageIndex', shopParams.pageNumber);
     params = params.append('pageSize', shopParams.pageSize);
