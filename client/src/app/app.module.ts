34a40272d7a237396581d737099860065dc1ea88
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-23 11:50:04 +0000

    lazy load shop

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
@@ -21,7 +20,6 @@ import { HomeModule } from './home/home.module';
     TooltipModule.forRoot(),
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule
   ],
   providers: [],
