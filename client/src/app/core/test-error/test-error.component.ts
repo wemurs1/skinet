a76f4d8d195197e360d4257667d5f077b2916dc9
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-23 12:40:09 +0000

    client error handling set up

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.scss'
+})
+export class TestErrorComponent {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/fourtytwo').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+}
