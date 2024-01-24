fe691f33ab656e310751383a65225e90c9e2bf2b
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-24 13:00:17 +0000

    tidy breadcrumbs

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,11 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private breadcrumbService: BreadcrumbService
+  ) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -20,7 +25,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) this.shopService.getProductById(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product;
+        this.breadcrumbService.set('@productDetails', product.name);
+      },
       error: error => console.log(error)
     });
   }
