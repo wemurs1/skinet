697b3b97453243c94a340380b386c902e034a5a8
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-26 12:30:40 +0000

    loading spinners

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -1,12 +1,17 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, delay } from 'rxjs';
+import { Observable, delay, finalize } from 'rxjs';
+import { BusyService } from '../services/busy.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private busyService: BusyService) { }
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000)
+      delay(1000),
+      finalize(() => this.busyService.idle())
     );
   }
 }
