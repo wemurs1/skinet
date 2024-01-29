bca9f3cdab4c27a2e2f661df83bc9d06715a37dc
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-29 18:43:16 +0000

    idenitity setup on API

---
+++ Core/Entities/Identity/Address.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities.Identity;
+
+public class Address
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Street { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? ZipCore { get; set; }
+
+    [Required]
+    public string? AppUserId { get; set; }
+    public AppUser? AppUSer { get; set; }
+}
---
+++ Core/Entities/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Entities.Identity;
+
+public class AppUser : IdentityUser
+{
+    public string? DisplayName { get; set; }
+    public Address? Address { get; set; }
+}
