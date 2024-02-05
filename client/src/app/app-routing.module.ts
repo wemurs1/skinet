6c60f6bfcb7b10469c8ee611be198083140b3b4e
Stuart Murray <stuart.murray.dev@outlook.com>
2024-02-05 10:24:02 +0000

    order retrieval

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule) },
   { path: 'checkout', canActivate: [AuthGuard], loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule) },
+  { path: 'orders', canActivate: [AuthGuard], loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' } },
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
