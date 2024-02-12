ef664fca8151dd4784016beb5dcd3b24478236c5
Stuart Murray <stuart.murray.dev@outlook.com>
2024-02-12 08:55:24 +0000

    styling adjustment

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -10,7 +10,8 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (
       request.url.includes('emailexists') ||
-      request.method === 'POST' && request.url.includes('orders')
+      request.method === 'POST' && request.url.includes('orders') ||
+      request.method === 'DELETE'
     ) {
       return next.handle(request);
     }
